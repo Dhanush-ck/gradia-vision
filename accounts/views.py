3b43d8c62c8a832e3282b4b9bbce5e0b362a67ca
Dhanush CK <dhanushck01@gmail.com>
2025-11-20 23:48:11 +0530

    feat: Changed the view name and added role identification

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
 
-def signup_page(request):
-    return render(request, 'signup.html')
+def select_role(request):
+    if request.method == 'POST':
+            role = request.POST.get('role')
+
+            if role == 'student':
+                print("student")
+            elif role == 'tutor':
+                print("tutor")
+            else:
+                print("admin")
+
+    return render(request, 'role.html')
