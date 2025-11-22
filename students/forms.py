dd54f0dd0e1505675b4ba9e9c20b257848860d7c
Dhanush CK <dhanushck01@gmail.com>
2025-11-23 01:07:29 +0530

    feat: Created a form for student signin

--- students/forms.py
+++ students/forms.py
@@ -6,3 +6,7 @@ class StudentForm(forms.Form):
     semester = forms.IntegerField(min_value=1, max_value=8, widget=forms.TextInput(attrs={'placeholder': 'Enter your semester', 'id': 'semester'}))
     password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password' ,'id':'password'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':"Enter your email", 'id':'email'}))
+
+class StudentSigninForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':"Enter your email", 'id':'email'}))
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password' ,'id':'password'}))
